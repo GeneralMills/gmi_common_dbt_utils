308afdd674f9797e48162a28489279b4e9013910
Pinaki Das <pinaki.das@genmills.com>
2023-08-25 19:39:55 +0530

    Update smart_source.sql
    Setting a default value for DBT_POPULATE_DEV_TABLES

--- macros/smart_source.sql
+++ macros/smart_source.sql
@@ -1,7 +1,12 @@
 {% macro smart_source(source_name, table_name) %}
-  {%- if env_var('DBT_RUN_ENV') | upper == 'DEV' and env_var('DBT_USE_ZDEV_DATASET') | upper == 'TRUE' -%}
-    (select * from {{source(source_name,table_name) }} where 1 = 0) as __dbt_source_{{ table_name }} 
+  {%- set dbt_populate_dev_tables = env_var('DBT_POPULATE_DEV_TABLES','FALSE') -%}
+  {%- if env_var('DBT_RUN_ENV') == 'DEV' -%}
+    {%- if dbt_populate_dev_tables | upper == 'TRUE'-%}
+      {{source(source_name, table_name)}}
+    {% else %}
+      (select * from {{source(source_name,table_name) }} where 1 = 0) as __dbt_source_{{ table_name }} 
+    {% endif %}
   {% else %}
     {{source(source_name, table_name)}}
   {% endif %}
-{% endmacro %}
+	
