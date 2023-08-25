3e335995a51e5c0d790d6d4dd45f5a7a2e48af84
Pinaki Das <pinaki.das@genmills.com>
2023-08-25 13:10:42 +0530

    Update smart_source.sql
    updated smart_source macro to consider the DBT_USE_ZDEV_DATASET environment variable

--- macros/smart_source.sql
+++ macros/smart_source.sql
@@ -1,11 +1,6 @@
 {% macro smart_source(source_name, table_name) %}
-  {%- set dbt_populate_dev_tables = env_var('DBT_POPULATE_DEV_TABLES') -%}
-  {%- if env_var('DBT_RUN_ENV') == 'DEV' -%}
-    {%- if dbt_populate_dev_tables is defined and dbt_populate_dev_tables | upper == 'TRUE'-%}
-      {{source(source_name, table_name)}}
-    {% else %}
-      (select * from {{source(source_name,table_name) }} where 1 = 0) as __dbt_source_{{ table_name }} 
-    {% endif %}
+  {%- if env_var('DBT_RUN_ENV') | upper == 'DEV' and env_var('DBT_USE_ZDEV_DATASET') | upper == 'TRUE' -%}
+    (select * from {{source(source_name,table_name) }} where 1 = 0) as __dbt_source_{{ table_name }} 
   {% else %}
     {{source(source_name, table_name)}}
   {% endif %}
