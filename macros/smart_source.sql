be41ed0fabb4ae31ec32f634c3865d0e1036d274
Pinaki Das <pinaki.das@genmills.com>
2023-08-25 19:43:05 +0530

    Update smart_source.sql
    fix syntax error

--- macros/smart_source.sql
+++ macros/smart_source.sql
@@ -1,7 +1,7 @@
 {% macro smart_source(source_name, table_name) %}
   {%- set dbt_populate_dev_tables = env_var('DBT_POPULATE_DEV_TABLES','FALSE') -%}
-  {%- if env_var('DBT_RUN_ENV') == 'DEV' -%}
-    {%- if dbt_populate_dev_tables | upper == 'TRUE'-%}
+  {%- if env_var('DBT_RUN_ENV') | upper == 'DEV' -%}
+    {%- if dbt_populate_dev_tables | upper == 'TRUE' -%}
       {{source(source_name, table_name)}}
     {% else %}
       (select * from {{source(source_name,table_name) }} where 1 = 0) as __dbt_source_{{ table_name }} 
